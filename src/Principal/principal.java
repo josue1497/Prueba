ded0a8624ec3e261330a95c94e5459ca2959a51a
josue1497 <jrb.11@hotmail.com>
2017-09-21 16:10:43 -0400

    Initial commit

---
+++ src/Principal/principal.java
@@ -0,0 +1,17 @@
+
+package Principal;
+
+import Vista.ventana_principal;
+
+
+public class principal {
+
+    
+    public static void main(String[] args) {
+        
+        
+        ventana_principal ver=new ventana_principal();
+        
+    }
+    
+}
